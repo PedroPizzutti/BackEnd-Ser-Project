d53cf72c33859e6ff9e292114e6563c2eb91b780
Pedro Pizzutti <pedropizzutti@gmail.com>
2023-06-03 10:31:28 -0300

    feat: implemented the methods in the APP class

--- api/src/App.pas
+++ api/src/App.pas
@@ -4,6 +4,7 @@ interface
 
 uses
   System.SysUtils,
+  Winapi.Windows,
   Horse,
   Horse.CORS,
   Horse.Jhonson,
@@ -13,6 +14,9 @@ type
   TApp = class
   private
     var FApp: THorse;
+    procedure RegisterMiddlewares;
+    procedure RegisterRoutes;
+    procedure PrintInfo;
   public
     procedure Start(const APort: Int64);
     constructor Create;
@@ -25,7 +29,10 @@ implementation
 
 constructor TApp.Create;
 begin
-  //TODO
+  Self.FApp := THorse.Create;
+  Self.RegisterMiddlewares;
+  Self.RegisterRoutes;
+  ReportMemoryLeaksOnShutdown := True;
 end;
 
 destructor TApp.Destroy;
@@ -34,12 +41,35 @@ begin
   inherited;
 end;
 
+procedure TApp.PrintInfo;
+begin
+  Writeln('--- SerPrestadores-Api ---');
+  Writeln(Format('Server on: %s', [FApp.Host]));
+  Writeln(Format('Port: %d', [FApp.Port]));
+  Writeln('Press ENTER to stop the application...');
+  Readln;
+  THorse.StopListen;
+  FreeConsole;
+end;
+
+procedure TApp.RegisterMiddlewares;
+begin
+  Self.FApp.Use(Jhonson);
+  Self.FApp.Use(CORS);
+  Self.FApp.Use(HorseSwagger);
+end;
+
+procedure TApp.RegisterRoutes;
+begin
+  //TODO
+end;
+
 procedure TApp.Start(const APort: Int64);
 begin
   try
-    FApp.Listen(APort);
+    FApp.Listen(APort, Self.PrintInfo);
   except on E: Exception do
-    raise Exception.Create('Initialization error: ' + E.Message);
+    Writeln('Initialization erro: ' + E.Message);
   end;
 end;
 
