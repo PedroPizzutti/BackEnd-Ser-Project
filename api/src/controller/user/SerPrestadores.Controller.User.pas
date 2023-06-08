2bd71f93d0c3f67e0d0ab0714a68c9e751c93fe4
Pedro Pizzutti <pedropizzutti@gmail.com>
2023-06-08 11:02:11 -0300

    feat: created the post route

--- api/src/controller/user/SerPrestadores.Controller.User.pas
+++ api/src/controller/user/SerPrestadores.Controller.User.pas
@@ -8,21 +8,24 @@ uses
   System.StrUtils,
   System.SysUtils,
   System.Types,
-  GBSwagger.Path.Attributes,
+  BCrypt,
   Horse,
   Horse.GBSwagger,
   Horse.Jhonson,
   Horse.Commons,
   Rest.Json,
+  GBSwagger.Path.Attributes,
   SerPrestadores.Model.Dao.GenericDAO,
   SerPrestadores.Model.User.Entity,
   SerPrestadores.Model.Error.Entity;
 
 type
   [SwagPath('users', 'Usuários')]
-  TControllerProvider = class(THorseGBSwagger)
+  TControllerUser = class(THorseGBSwagger)
     private
       var FDAO: IGenericDAO<TUserEntity>;
+      procedure ValidateUser(AJSONObject: TJSONObject);
+
     public
       [SwagPOST('', 'Create a user')]
       [SwagResponse(201, nil)]
@@ -33,11 +36,72 @@ type
 
 implementation
 
-{ TControllerProvider }
+{ TControllerUser }
 
-procedure TControllerProvider.Post;
+procedure TControllerUser.Post;
+var
+  LPassword: String;
+  LEncryptedPassword: String;
+  LJSONRequest: TJSONObject;
 begin
-  //TODO
+  LJSONRequest := FRequest.Body<TJSONObject>;
+
+  Self.ValidateUser(LJSONRequest);
+
+  LPassword := LJSONRequest.GetValue<String>;
+  LEncryptedPassword := TBCrypt.GenerateHash(LPassword);
+  LJSONRequest.Get('password').JsonValue := TJSONString.Create(LEncryptedPassword);
+
+  FDAO := TGenericDAO<TUserEntity>.New;
+  FDAO.Insert(LJSONRequest);
+
+  FResponse.Status(THTTPStatus.Created);
+end;
+
+procedure TControllerUser.ValidateUser(AJSONObject: TJSONObject);
+var
+  LName: String;
+  LEmail: String;
+  LPassword: String;
+  LNullFieldsList: TStringList;
+begin
+  LNullFieldsList := TStringList.Create;
+  try
+    if not AJSONObject.TryGetValue<String>('name', LName) then
+    begin
+      LNullFieldsList.Add('name');
+    end;
+
+    if not AJSONObject.TryGetValue<String>('email', LEmail) then
+    begin
+      LNullFieldsList.Add('email');
+    end;
+
+    if not AJSONObject.TryGetValue<String>('password', LPassword) then
+    begin
+      LNullFieldsList.Add('password');
+    end;
+
+    if LNullFieldsList.Count > 0 then
+    begin
+      var LMsg := '';
+      if LNullFieldsList.Count = 1 then
+      begin
+        LMsg := 'campo obrigatório: ' + LNullFieldsList.Text;
+      end
+      else
+      begin
+        LMsg := 'campos obrigatórios: ' + LNullFieldsList.Text;
+      end;
+
+      raise EHorseException.New.Error(LMsg).Status(THTTPStatus.BadRequest);
+    end;
+
+  finally
+    LNullFieldsList.DisposeOf;
+  end;
+
+
 end;
 
 end.
