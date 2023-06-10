80ca3b4fa3178b697ad43d1624d05cfdf7b2c3c0
Pedro Pizzutti <pedropizzutti@gmail.com>
2023-06-10 15:38:04 -0300

    feat: created the function GenerateToken in the Utils

--- api/src/utils/SerPrestadores.Utils.pas
+++ api/src/utils/SerPrestadores.Utils.pas
@@ -8,11 +8,16 @@ uses
   System.StrUtils,
   System.JSON,
   BCrypt,
-  Horse;
+  Horse,
+  Horse.JWT,
+  JOSE.Types.Bytes,
+  JOSE.Core.JWT,
+  JOSE.Core.Builder;
 
 type
   TUtils = class
   public
+    class function GenerateToken(AIdUser: String): String;
     class procedure EncryptPasswordJSON(var AJSONObject: TJSONObject; const APasswordField: String);
     class procedure ValidateId(const AId: Int64);
     class procedure ValidateFieldsString(const AJSONObject: TJSONObject; const AFieldList: TStringList);
@@ -37,6 +42,22 @@ begin
     end;
   except on E: Exception do
     raise EHorseException.New.Error('encrypt error - ' + E.Message).Status(THTTPStatus.InternalServerError);
+  end;
+end;
+
+class function TUtils.GenerateToken(AIdUser: String): String;
+var
+  LJWT: TJWT;
+begin
+  LJWT := TJWT.Create;
+  try
+    LJWT.Claims.Issuer := 'SerPrestadores';
+    LJWT.Claims.Subject := AIdUser;
+    LJWT.Claims.Expiration := Now + 1;
+
+    Result := TJOSE.SHA256CompactToken('SER', LJWT);
+  finally
+    LJWT.DisposeOf;
   end;
 end;
 
